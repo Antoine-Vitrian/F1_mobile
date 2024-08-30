6000fa5b3dfe2cdda886041cb493a6b8349b6f17
Victor Antonio Teixeira da Silva <165703139+Antoine-Vitrian@users.noreply.github.com>
2024-08-30 16:43:32 -0300

    Add files via upload
    testando transicao de pilotos

---
+++ f1_test.js
@@ -0,0 +1,11 @@
+const albon = document.getElementById('albon');
+const colapinto = document.getElementById('colapinto');
+
+function mostrarAlbon() {
+     albon.style.display = 'block';
+    colapinto.style.display = 'none';
+}
+function mostrarColapinto() {
+    albon.style.display = 'none';
+    colapinto.style.display = 'block';
+}
